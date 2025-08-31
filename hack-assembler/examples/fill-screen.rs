03688bfcb4f0f71e457acdd8fcd7b44e0093d1cf
elvis.pysqyli <elvis.pysqyli@gmail.com>
2025-08-31 14:35:03 +0200

    examples: handle workspace paths

--- hack-assembler/examples/fill-screen.rs
+++ hack-assembler/examples/fill-screen.rs
@@ -1,11 +1,19 @@
-use std::fs;
+use std::{env, fs};
 
 use hack_assembler::assembler::Assembler;
 
 fn main() {
-    match Assembler::from_file("hack-assembler/examples/fill-screen.asm") {
+    let mut asm_file_path = "examples/fill-screen.asm";
+    let mut hack_file_path = "examples/fill-screen.hack";
+
+    if env::current_dir().unwrap().ends_with("jack-to-hack") {
+        asm_file_path = "hack-assembler/examples/fill-screen.asm";
+        hack_file_path = "hack-assembler/examples/fill-screen.hack";
+    }
+
+    match Assembler::from_file(asm_file_path) {
         Ok(assembler) => match assembler.compile() {
-            Ok(hack) => match fs::write("hack-assembler/examples/fill-screen.hack", hack.join("\n")) {
+            Ok(hack) => match fs::write(hack_file_path, hack.join("\n")) {
                 Ok(_) => println!("fill-screen compiled to hack"),
                 Err(err) => eprintln!("{err}"),
             },
--- vm-translator/examples/addition-from-stack.rs
+++ vm-translator/examples/addition-from-stack.rs
@@ -1,5 +1,13 @@
+use std::env;
+
 use vm_translator::translate_vm_program_to_file;
 
 fn main() {
-    translate_vm_program_to_file("vm-translator/examples/push-and-add.vm");
+    let vm_file_path = if env::current_dir().unwrap().ends_with("jack-to-hack") {
+        "vm-translator/examples/push-and-add.vm"
+    } else {
+        "examples/push-and-add.vm"
+    };
+
+    translate_vm_program_to_file(vm_file_path);
 }
