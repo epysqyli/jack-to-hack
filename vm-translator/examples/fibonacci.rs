059861e37a447389a1a56ae9420f64299d89f398
elvis.pysqyli <elvis.pysqyli@gmail.com>
2025-09-27 11:23:11 +0200

    vm-translator: add fibonacci example

--- vm-translator/examples/fibonacci.rs
+++ vm-translator/examples/fibonacci.rs
@@ -4,13 +4,12 @@ use vm_translator::translate_vm_from_path;
 
 fn main() {
     let vm_file_path = if env::current_dir().unwrap().ends_with("jack-to-hack") {
-        "vm-translator/examples/push-and-add.vm"
+        "vm-translator/examples/fibonacci"
     } else {
-        "examples/push-and-add.vm"
+        "examples/fibonacci"
     };
 
     let asm = translate_vm_from_path(&PathBuf::from(vm_file_path));
 
-    fs::write(vm_file_path.replace(".vm", ".asm"), asm.join("\n"))
-        .expect("Writing asm to file failed");
+    fs::write("source.asm", asm.join("\n")).expect("Writing asm to file failed");
 }
