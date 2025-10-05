8e7f19f60ad5d778d1a46087d9de1a0d6c10c313
elvis.pysqyli <elvis.pysqyli@gmail.com>
2025-10-05 17:37:02 +0200

    parser: init

---
+++ jack-to-vm/src/syntax-analyzer/parser.rs
@@ -0,0 +1,10 @@
+// The parser:
+// - takes in an input stream of tokens
+// - recursively applies a set of grammar rules to it
+// - produces a derivation/parse tree or fails if grammer rules are not applicable
+
+use crate::syntax_analyzer::tokenizer::Token;
+
+pub fn parse(tokens: Vec<Token>) {
+    // TODO: implement
+}
